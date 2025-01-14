740eb31ab44208e4e56b667af3686e596dafae37
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2025-01-14 16:31:55 +0200

    feat: added a the chart for end user

--- src/app/eu/dashboard/page.tsx
+++ src/app/eu/dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import { Icon } from "@iconify/react/dist/iconify.js";
 import styles from "./page.module.css";
+import Chart from "@/components/Chart";
 
 export default function Dashboard() {
   return (
@@ -15,6 +16,74 @@ export default function Dashboard() {
           <button className={styles.notificationButton}>
             <Icon icon="simple-line-icons:bell" width="30" height="25" />
           </button>
+        </div>
+      </div>
+      <div className={styles.mainSection}>
+        <div className={styles.mainLeft}>
+          <div className={styles.cardSection}>
+            <div className={styles.cardContainer}>
+              <Icon
+                icon="ph:thumbs-up"
+                width="25"
+                height="25"
+                color="#242e8f"
+              />
+              <div className={styles.cardTextContainer}>
+                <p className={styles.cardText}>Approved requests</p>
+                <div className={styles.cardInfo}>
+                  <p className={styles.approved}>23</p>
+                  <div className={styles.cardTextApproved}>
+                    <Icon icon="ph:caret-up-fill" width="25" height="25" />
+                    <p>4 requests</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.cardContainer}>
+              <Icon
+                icon="ph:thumbs-down"
+                width="25"
+                height="25"
+                color="#242e8f"
+              />
+              <div className={styles.cardTextContainer}>
+                <p className={styles.cardText}>Denied requests</p>
+                <div className={styles.cardInfo}>
+                  <p className={styles.approved}>4</p>
+                  <div className={styles.cardTextDenied}>
+                    <Icon icon="ph:caret-up-fill" width="25" height="25" />
+                    <p>2 requests</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.cardContainerLast}>
+              <Icon
+                icon="ph:arrows-clockwise"
+                width="25"
+                height="25"
+                color="#242e8f"
+              />
+              <div className={styles.cardTextContainer}>
+                <p className={styles.cardText}>Pending requests</p>
+                <div className={styles.cardInfo}>
+                  <p className={styles.approved}>11</p>
+                  <div className={styles.cardTextDenied}>
+                    <Icon
+                      icon="ph:dots-three-circle-light"
+                      width="25"
+                      height="25"
+                      color="rgba(0,0,0,0.8)"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.chartSection}>
+            
+            <Chart />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Chart.js
@@ -0,0 +1,60 @@
+"use client"
+
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card"
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+  { month: "July", desktop: 100, mobile: 10 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Approved",
+    color: "hsl(var(--chart-2))",
+  },
+  mobile: {
+    label: "Denied",
+    color: "hsl(var(--chart-1))",
+  },
+}
+
+export default function Component() {
+  return (
+    <Card>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="dashed" />}
+            />
+            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={2} />
+            <Bar dataKey="mobile" fill="var(--color-mobile)" radius={2} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  )
+}
