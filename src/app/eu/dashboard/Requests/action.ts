822869512939f639c9362ce2a7b961753bc09624
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2025-04-10 22:13:36 +0200

    feat: added the request list page

---
+++ src/app/eu/dashboard/Requests/action.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import axios from "axios";
+import { unstable_noStore as noStore } from "next/cache";
+
+interface Config {
+  backend: string;
+}
+
+const config: Config = {
+  backend: process.env.BACKEND_LINK || "http://localhost:3001",
+};
+
+const navigateNewRequest = () => {
+  redirect("/eu/dashboard/Requests/new-request");
+};
+
+const getRequests = async () => {
+  const postLink = `${config.backend}request/get/dID`;
+  try {
+    const response = await axios.get(postLink);
+    console.log(response.data);
+    noStore();
+    return { success: true, data: response.data };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return { success: false, message: "Error:", data: null };
+  }
+};
+
+export { navigateNewRequest, getRequests };
--- src/app/eu/dashboard/Requests/page.tsx
+++ src/app/eu/dashboard/Requests/page.tsx
@@ -1,9 +1,95 @@
 "use client";
 
 import styles from "./page.module.css";
+import { navigateNewRequest } from "./action";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import DataTable from "react-data-table-component";
+// import { useEffect, useState } from "react";
+
+interface Row {
+  title: string;
+  patientName: string;
+  priority: string;
+  status: string;
+  requestId: number;
+}
+
+const columns = [
+  {
+    name: "Title",
+    selector: (row: Row) => row.title,
+    sortable: true,
+  },
+  {
+    name: "Patient",
+    selector: (row: Row) => row.patientName,
+    sortable: true,
+  },
+  {
+    name: "Priority",
+    selector: (row: Row) => row.priority,
+    sortable: true,
+  },
+  {
+    name: "Status",
+    selector: (row: Row) => row.status,
+    sortable: true,
+  },
+  //   {
+  //     name: "Actions",
+  //     cell: (row: Row) => (
+  //       <button
+  //         onClick={() => handleUpdateArticle(row.requestId)}
+  //         className="update-container"
+  //       ></button>
+  //     ),
+  //     button: true,
+  //   },
+];
 
 export default function Requests() {
-    return (
-        <div className={styles.header}>Requests</div>
-    );
+  //     const [records, setRecords] = useState([])
+
+  //   useEffect(() => {
+  //     const getInitialData = () => {
+  //         setRecords(getRequests.data)
+  //     }
+  //   }, []);
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerSection}>
+        <div className={styles.headerLeftSection}>
+          <p>Welcome back, Dr Bayingana</p>
+        </div>
+        <button className={styles.newAppealButton} onClick={navigateNewRequest}>
+          New Request
+        </button>
+      </div>
+      <div className={styles.searchBar}>
+        <input
+          className={styles.searchField}
+          placeholder="Search by title........"
+        />
+        <button className={styles.searchButton}>
+          <Icon
+            icon="ph:magnifying-glass-bold"
+            width="30"
+            height="30"
+            color="#242e8f"
+          />
+        </button>
+      </div>
+      <div className={styles.tableContainer}>
+        <DataTable
+          className="table"
+          columns={columns}
+          data={[]}
+          selectableRows
+          fixedHeader
+          pagination
+        />
+      </div>
+    </div>
+  );
 }
